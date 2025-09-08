f6122e8f84bae0f36308d7041fee2dc8a9a85f4f
PsGhosh <pgsarothi2005@gmail.com>
2025-09-08 21:38:35 +0600

    first

---
+++ code.c
@@ -0,0 +1,25 @@
+// All information and code for WHILE loops
+#include <stdio.h>  
+int main (){
+    int i=3;
+    while (i<=5){
+        printf("%d\n",i);
+        i++;
+    }
+
+    return 0;
+}
+// Output:
+// 3    
+// 4
+// 5
+// Explanation: The loop starts with i=3 and continues as long as i is less than or equal to 5.
+// Each iteration prints the current value of i and then increments it by 1.
+
+// Note: This code is written in C17 standard as specified.
+// The code demonstrates the use of a WHILE loop to print numbers from 3 to 5.
+// The loop will execute as long as the condition (i <= 5) is true.
+// The loop will print the values 3, 4, and 5 on separate lines.
+// The loop stops when i becomes 6, which does not satisfy the condition anymore.
+// The code is self-contained and can be compiled and run in any standard C environment.
+// All information and code for WHILE loops
